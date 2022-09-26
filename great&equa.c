5e6bf75accc8dc29ef6f87d3641506fedb2418d8
sushantbansal720 <sushantbansal720@gmail.com>
2022-09-26 14:44:48 +0530

    1

---
+++ great&equa.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main ()
+{
+    int a,b;
+    printf("Enter value for a:");
+    scanf("%d", &a);
+
+    printf("Enter value for b:");
+    scanf("%d", &b);
+
+    if(a>b)
+    {
+        printf
+    }
+}
